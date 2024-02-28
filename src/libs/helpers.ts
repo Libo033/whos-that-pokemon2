9b976ea305892092bc83446f902d11f6bc3e868f
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-02-28 15:48:04 -0300

    Creadas: Funciones getRandomInt y countdown

--- src/libs/helpers.ts
+++ src/libs/helpers.ts
@@ -1,7 +1,32 @@
-export const storeName = (name: string) => {
+import React, { SetStateAction } from "react";
+
+export const storeName = (name: string): void => {
   localStorage.setItem("Username", name);
 };
 
-export const deleteStoreName = () => {
+export const deleteStoreName = (): void => {
   localStorage.removeItem("Username");
 };
+
+export function getRandomInt(min: number, max: number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+export function InicioCuentaRegresiva(
+  time: number,
+  setCountdown: React.Dispatch<SetStateAction<number>>
+) {
+  const inicio = Date.parse(new Date().getMilliseconds().toString());
+
+  setInterval(() => {
+    const total = Date.parse(new Date().getMilliseconds().toString());
+
+    if (inicio / 1000 - total / 1000 + time >= 0) {
+      setCountdown(inicio / 1000 - total / 1000 + time);
+    } else {
+      return;
+    }
+  }, 1000);
+}
