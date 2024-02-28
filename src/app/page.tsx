f627374527cb3b0c89987286bceb3fd5f6b811ab
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-02-28 11:18:47 -0300

    Agregado: background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,16 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <>
-
-    </>
+    <main className="inicio">
+      <Image
+        className="inicio-background"
+        src={
+          "https://res.cloudinary.com/dsuydyqgz/image/upload/v1709129158/01-varios/oihosidf7lvq7fwjmv05.png"
+        }
+        alt="pokemon background"
+        width={748}
+        height={421}
+      />
+    </main>
   );
 }
